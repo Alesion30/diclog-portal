01fced2bb55500713494f10e4a83f9457a094de8
Yuta Ohira <alesion90@gmail.com>
2021-12-02 16:29:41 +0900

    setup: `mui`を導入

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,14 @@ const MyDocument: React.VFC = () => {
         <title>{title}</title>
         <meta name="description" content={description} />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
       </Head>
       <body>
         <Main />
