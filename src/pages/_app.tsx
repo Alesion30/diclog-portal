894dff4560348fab79caca07c81fd60250efd5ce
Yuta Ohira <alesion90@gmail.com>
2021-12-02 15:15:08 +0900

    add: `_app.tsx`と`_document.tsx`を追加

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import type { AppProps } from 'next/app'
+
+const MyApp: React.VFC<AppProps> = ({ Component, pageProps }) => {
+    return <Component {...pageProps} />
+}
+
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
-import Head from 'next/head'
 import Image from 'next/image'
 
 export const Home = (): JSX.Element => (
   <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
     <main>
       <h1 className="title">
         Welcome to <a href="https://nextjs.org">Next.js!</a>
