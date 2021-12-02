b623010943b78c2ad8ae393e50f9f792fbb76796
Yuta Ohira <alesion90@gmail.com>
2021-12-02 15:15:41 +0900

    fix: format

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from 'react'
 import type { AppProps } from 'next/app'
 
 const MyApp: React.VFC<AppProps> = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from 'react'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 const MyDocument: React.VFC = () => {
-    const title = 'diclog';
-    const description = '英単語学習サプリメントシステム';
+  const title = 'diclog'
+  const description = '英単語学習サプリメントシステム'
 
-    return (
-        <Html lang="ja">
-            <Head>
-                <title>{title}</title>
-                <meta name="description" content={description} />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+  return (
+    <Html lang="ja">
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
 
-export default MyDocument;
+export default MyDocument
