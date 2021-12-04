4a270ebf14be87aa3a0c355fbad267bb1529c457
Yuta Ohira <alesion90@gmail.com>
2021-12-04 12:16:06 +0900

    setup: aliasを設定

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -13,6 +15,13 @@ module.exports = {
       },
     },
   ],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "~": path.resolve(__dirname, '../src')
+    }
+    return config
+  },
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
--- src/stories/components/Header.stories.tsx
+++ src/stories/components/Header.stories.tsx
@@ -1,4 +1,4 @@
-import { Header } from '../../components/Header'
+import { Header } from '~/components/Header'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 export default {
