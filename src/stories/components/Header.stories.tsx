07b9f73d80f2db73dbdd50ca4c6bea3837a4624a
Yuta Ohira <alesion90@gmail.com>
2021-12-04 12:12:56 +0900

    add: ヘッダーのストーリーを追加

---
+++ src/stories/components/Header.stories.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../../components/Header'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+export default {
+  title: 'Header',
+  component: Header,
+} as ComponentMeta<typeof Header>
+
+const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />
+
+export const Default = Template.bind({})
