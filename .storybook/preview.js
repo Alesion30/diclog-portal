c6b4eb434c0ea01c390dbf0f2f96b445075427b4
Yuta Ohira <alesion90@gmail.com>
2021-12-04 12:44:21 +0900

    remove: `mui`を除去

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,4 @@
-import { muiTheme } from 'storybook-addon-material-ui5'
 import '../src/styles/global.css'
-
-export const decorators = [muiTheme()]
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
