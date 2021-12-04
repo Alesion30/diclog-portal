d2fe64871f139121c0f685d8bcddac996cd98c55
Yuta Ohira <alesion90@gmail.com>
2021-12-04 15:28:52 +0900

    setup: `Noto Sans JP`を設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,14 @@ const MyDocument: React.VFC = () => {
           rel="stylesheet"
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
         />
+
+        {/* Googleフォント */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
