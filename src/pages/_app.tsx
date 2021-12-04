b9e04b32aad04c1a33b24f816268f9ef60bc99b7
Yuta Ohira <alesion90@gmail.com>
2021-12-04 23:03:53 +0900

    add: 画面遷移時のアニメーションを実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
 import '~/styles/global.css'
+import { AnimatePresence } from 'framer-motion'
 
 const MyApp: React.VFC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <AnimatePresence exitBeforeEnter initial={true}>
+      <Component {...pageProps} />
+    </AnimatePresence>
+  )
 }
 
 export default MyApp
