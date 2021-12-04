bb78a688c4ba1e6b664f9b0e9c673f4b17fc4508
Yuta Ohira <alesion90@gmail.com>
2021-12-04 11:25:45 +0900

    setup: `sass`に対応

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -5,6 +7,14 @@ module.exports = {
     '@storybook/addon-essentials',
     'storybook-addon-material-ui5',
   ],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+    return config;
+  },
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
