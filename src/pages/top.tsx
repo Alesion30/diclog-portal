bc1ca8d07b66f722241d22d43168ebef78640c8f
Yuta Ohira <alesion90@gmail.com>
2022-01-13 22:10:50 +0900

    :lipstick: Diclogとは？を追加

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -2,13 +2,13 @@ import { NextPage } from 'next'
 import Image from 'next/image'
 import { Button } from '~/components/Button'
 import { Container } from '~/components/Container'
-import { IconImage } from '~/components/Image'
+import { DummyImage, IconImage } from '~/components/Image'
 
 const TopPage: NextPage = () => {
   return (
     <div className="">
       {/* ヘッダー */}
-      <Container className="w-full fixed top-5">
+      <Container className="w-full fixed top-5 z-10">
         <header className="bg-white justify-between content-center rounded-full flex px-10 py-5 shadow-lg">
           <div className="flex space-x-10 items-center">
             <div className="flex space-x-2 content-center">
@@ -34,7 +34,7 @@ const TopPage: NextPage = () => {
       </Container>
 
       {/* TOP */}
-      <div className="h-screen">
+      <div className="min-h-screen">
         <Container>
           <div className="flex justify-evenly items-center h-screen">
             <div className="space-y-8">
@@ -59,6 +59,51 @@ const TopPage: NextPage = () => {
               objectFit="contain"
               alt="画像"
             />
+          </div>
+        </Container>
+      </div>
+
+      {/* Diclogとは？ */}
+      <div className="min-h-screen bg-yellow-50">
+        <Container>
+          <div className="flex flex-col justify-center items-center py-28 space-y-10">
+            <div className="space-y-10 text-center">
+              <h1 className="text-5xl font-bold">
+                <span className="font-lemon">Diclog</span>とは？
+              </h1>
+              <p className="text-lg leading-relaxed whitespace-pre-wrap text-gray-700">
+                Diclogは自分の経験に基づいて、『こないだ調べたあの単語』を『復習』することによって記憶の定着を図る英単語帳です。
+                <br />
+                しかも、単語登録の手間を完全に自動化してあります。DropboxやEvernoteを使っている人なら、自動同期の快適さは理解できると思います。
+                <br />
+                最初にChrome
+                Extensionを入れる必要がありますが、その後はそのExtensionが自動的に調べた英単語を記録してくれます。
+                <br />
+                あとは、スマホでチェックして、消していくだけです。チェックするのは、トイレでも電車でも、ほんのちょっとした隙間時間です。
+              </p>
+            </div>
+            <div className="flex flex-wrap justify-center">
+              {[1, 2, 3, 4].map((_, index) => {
+                return (
+                  <div
+                    key={index}
+                    style={{ width: 520 }}
+                    className="bg-white shadow-lg rounded-md flex justify-between px-8 py-12 m-5"
+                  >
+                    <div style={{ width: 232 }} className="space-y-3">
+                      <h2 className="text-lg font-bold">
+                        自分だけのMy単語帳が出来上がります
+                      </h2>
+                      <p className="text-base leading-relaxed whitespace-pre-wrap text-gray-700">
+                        Chrome&nbsp;
+                        Extensionと連携するだけで、単語を調べていくうちにあなただけの単語帳が勝手に出来上がります。
+                      </p>
+                    </div>
+                    <DummyImage width={200} />
+                  </div>
+                )
+              })}
+            </div>
           </div>
         </Container>
       </div>
