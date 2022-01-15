0d0bcc4c7ed6d2590de6353123de3eeff185fd11
Yuta Ohira <alesion90@gmail.com>
2022-01-15 23:17:46 +0900

    :lipstick: カードめくりを実装

--- src/components/DiclogApp/index.tsx
+++ src/components/DiclogApp/index.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react'
+import ReactCardFlip from 'react-card-flip'
 import { IPhoneBoard } from '../iPhoneBoard'
 
 export const DiclogApp: React.VFC = () => {
+  const [isFlipped, setIsFlipped] = useState<boolean>(false)
+
   return (
     <IPhoneBoard width={300}>
       <div className="flex flex-col h-full">
@@ -8,9 +12,22 @@ export const DiclogApp: React.VFC = () => {
           <p className="text-base">ランダムで出題</p>
         </div>
         <div className="pt-2 pb-7 px-2 h-full">
-          <div className="bg-white flex justify-center items-center border rounded-lg shadow-lg w-full h-full">
-            <p className="text-4xl">Hello</p>
-          </div>
+          <ReactCardFlip isFlipped={isFlipped} containerClassName="h-full">
+            <div
+              key="front"
+              onClick={() => setIsFlipped(true)}
+              className="bg-white flex justify-center items-center border rounded-lg shadow-lg w-full h-full"
+            >
+              <p className="text-4xl select-none">Hello</p>
+            </div>
+            <div
+              key="back"
+              onClick={() => setIsFlipped(false)}
+              className="bg-white flex justify-center items-center border rounded-lg shadow-lg w-full h-full"
+            >
+              <p className="text-4xl select-none">こんにちは</p>
+            </div>
+          </ReactCardFlip>
         </div>
       </div>
     </IPhoneBoard>
