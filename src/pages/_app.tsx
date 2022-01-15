2f0cb368f5633ebf33b23f6d97d83b92939e3ac4
Yuta Ohira <alesion90@gmail.com>
2022-01-15 17:20:57 +0900

    :zap: next-seoの設定を外部ファイルに切り出し

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,34 +4,17 @@ import '~/styles/global.css'
 import '~/styles/loading.css'
 import { AnimatePresence } from 'framer-motion'
 import { DefaultSeo } from 'next-seo'
+import Head from 'next/head'
+import SEO from '../../next-seo.config'
 
 const MyApp: React.VFC<AppProps> = ({ Component, pageProps }) => {
-  const title = 'Diclog'
-  const description =
-    'Diclogは自分の経験に基づいて、『こないだ調べたあの単語』を『復習』することによって記憶の定着を図る英単語帳です。しかも、単語登録の手間を完全に自動化してあります。DropboxやEvernoteを使っている人なら、自動同期の快適さは理解できると思います。最初にChrome Extensionを入れる必要がありますが、その後はそのExtensionが自動的に調べた英単語を記録してくれます。あとは、スマホでチェックして、消していくだけです。チェックするのは、トイレでも電車でも、ほんのちょっとした隙間時間です。'
-  const url = 'https://diclog-portal.vercel.app'
-
   return (
     <AnimatePresence exitBeforeEnter initial={true}>
-      <DefaultSeo
-        defaultTitle={title}
-        canonical={url}
-        description={description}
-        openGraph={{
-          type: 'website',
-          title: title,
-          description: description,
-          site_name: title,
-          url: url,
-          images: [
-            {
-              url: '/assets/img/ogp.png',
-              width: 1280,
-              height: 640,
-            },
-          ],
-        }}
-      />
+      <Head>
+        <meta httpEquiv="x-ua-compatible" content="IE=edge; chrome=1" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </AnimatePresence>
   )
