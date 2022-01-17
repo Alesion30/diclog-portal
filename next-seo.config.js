edc33b5d4ccd91d3f6c3e05fdeffe54015b34614
Yuta Ohira <alesion90@gmail.com>
2022-01-17 13:08:38 +0900

    :wrench: OGPを修正

--- next-seo.config.js
+++ next-seo.config.js
@@ -5,15 +5,13 @@ export default {
   canonical: 'https://diclog-portal.vercel.app',
   noindex: false,
   openGraph: {
-    url: 'https://diclog-portal.vercel.app',
     title: 'Diclog（ディクログ）- まったく新しい英単語帳システム',
-    description:
-      'Diclogは自分の経験に基づいて、『こないだ調べたあの単語』を『復習』することによって記憶の定着を図る英単語帳です。しかも、単語登録の手間を完全に自動化してあります。DropboxやEvernoteを使っている人なら、自動同期の快適さは理解できると思います。最初にChrome Extensionを入れる必要がありますが、その後はそのExtensionが自動的に調べた英単語を記録してくれます。あとは、スマホでチェックして、消していくだけです。チェックするのは、トイレでも電車でも、ほんのちょっとした隙間時間です。',
+    description: 'Chrome拡張機能と連携し、自動であなただけの英単語帳を作成します。',
     images: [
       {
-        url: 'https://user-images.githubusercontent.com/50891407/149614656-f8b8e623-17c4-4c11-8e5d-9e3b7f698b3e.png',
+        url: '/ogp.png',
         width: 1280,
-        height: 640,
+        height: 720,
         alt: 'Diclog OGP Image',
       },
     ],
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,8 @@ const MyApp: React.VFC<AppProps> = ({ Component, pageProps }) => {
         <DefaultSeo {...SEO} />
         <Head>
           <link rel="icon" href="favicon.svg" type="image/svg+xml" />
+          <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" />
+
           <meta httpEquiv="x-ua-compatible" content="IE=edge; chrome=1" />
           <meta name="viewport" content="width=device-width,initial-scale=1" />
 
