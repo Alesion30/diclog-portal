74caba8b9d35a5fe12309c38507ad4ce454efb73
Yuta Ohira <alesion90@gmail.com>
2022-01-17 12:54:35 +0900

    :ambulance: prefixを設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 const MyDocument: React.VFC = () => {
   return (
-    <Html lang="ja">
+    <Html lang="ja" prefix="og: https://diclog-portal.vercel.app">
       <Head />
       <body>
         <Main />
