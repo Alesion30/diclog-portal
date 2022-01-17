8a763effd7225f2b807e0ccf810f33849b273b26
Yuta Ohira <alesion90@gmail.com>
2022-01-17 11:43:20 +0900

    :lipstick: ドットを追加

--- src/components/SlideShow/index.tsx
+++ src/components/SlideShow/index.tsx
@@ -52,58 +52,79 @@ export const SlideShow: React.VFC<SlideShowProps> = ({
     setPage([page + newDirection, newDirection])
   }
 
+  const movePage = (index: number) => {
+    setPage([index, 0])
+  }
+
   useInterval(() => {
     paginate(1)
   }, 5000)
 
   return (
-    <div
-      className={clsx('relative overflow-hidden', className)}
-      style={{ width, height: width / aspectRatio }}
-      {...props}
-    >
-      <AnimatePresence initial={false} custom={direction}>
-        <motion.img
-          key={`slide_show_${page}`}
-          src={images[imageIndex]}
-          custom={direction}
-          variants={variants}
-          initial="enter"
-          animate="center"
-          exit="exit"
-          transition={{
-            x: { type: 'spring', stiffness: 300, damping: 30 },
-            opacity: { duration: 0.2 },
-          }}
-          drag="x"
-          dragConstraints={{ left: 0, right: 0 }}
-          dragElastic={1}
-          onDragEnd={(e, { offset, velocity }) => {
-            const swipe = swipePower(offset.x, velocity.x)
+    <div className="flex flex-col items-center space-y-6">
+      <div
+        className={clsx('relative overflow-hidden', className)}
+        style={{ width, height: width / aspectRatio }}
+        {...props}
+      >
+        <AnimatePresence initial={false} custom={direction}>
+          <motion.img
+            key={`slide_show_${page}`}
+            src={images[imageIndex]}
+            custom={direction}
+            variants={variants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{
+              x: { type: 'spring', stiffness: 300, damping: 30 },
+              opacity: { duration: 0.2 },
+            }}
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            dragElastic={1}
+            onDragEnd={(e, { offset, velocity }) => {
+              const swipe = swipePower(offset.x, velocity.x)
 
-            if (swipe < -swipeConfidenceThreshold) {
-              paginate(1)
-            } else if (swipe > swipeConfidenceThreshold) {
-              paginate(-1)
-            }
-          }}
-          className="absolute"
-          alt="img"
-        />
-      </AnimatePresence>
-      <div
-        className="bg-white flex justify-center items-center w-8 h-8 rounded-full absolute text-2xl font-bold z-10 right-4 cursor-pointer select-none"
-        style={{ top: 'calc(50% - 20px)' }}
-        onClick={() => paginate(1)}
-      >
-        {'‣'}
+              if (swipe < -swipeConfidenceThreshold) {
+                paginate(1)
+              } else if (swipe > swipeConfidenceThreshold) {
+                paginate(-1)
+              }
+            }}
+            className="absolute"
+            alt="img"
+          />
+        </AnimatePresence>
+        <div
+          className="bg-white flex justify-center items-center w-8 h-8 rounded-full absolute text-2xl font-bold z-10 right-4 cursor-pointer select-none"
+          style={{ top: 'calc(50% - 20px)' }}
+          onClick={() => paginate(1)}
+        >
+          {'‣'}
+        </div>
+        <div
+          className="bg-white flex justify-center items-center w-8 h-8 rounded-full absolute text-2xl font-bold z-10 left-4 cursor-pointer select-none"
+          style={{ top: 'calc(50% - 20px)', transform: 'scale(-1)' }}
+          onClick={() => paginate(-1)}
+        >
+          {'‣'}
+        </div>
       </div>
-      <div
-        className="bg-white flex justify-center items-center w-8 h-8 rounded-full absolute text-2xl font-bold z-10 left-4 cursor-pointer select-none"
-        style={{ top: 'calc(50% - 20px)', transform: 'scale(-1)' }}
-        onClick={() => paginate(-1)}
-      >
-        {'‣'}
+      <div className="flex space-x-3">
+        {images.map((_, index) => {
+          const isActive = index === imageIndex
+          return (
+            <div
+              key={`slide_show_dot_${index}`}
+              className={clsx(
+                'w-3 h-3 rounded-full cursor-pointer',
+                isActive ? 'bg-yellow-500' : 'border border-yellow-500'
+              )}
+              onClick={() => movePage(index)}
+            />
+          )
+        })}
       </div>
     </div>
   )
