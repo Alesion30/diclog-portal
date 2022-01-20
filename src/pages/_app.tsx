de908e7950c04641344af03cbe8f4f210d572039
Yuta Ohira <alesion90@gmail.com>
2022-01-20 13:25:15 +0900

    :sparkles: react-queryの設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,46 +7,62 @@ import { DefaultSeo } from 'next-seo'
 import SEO from '../../next-seo.config'
 import Head from 'next/head'
 import { RecoilRoot } from 'recoil'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 const MyApp: React.VFC<AppProps> = ({ Component, pageProps }) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: true,
+        refetchOnWindowFocus: false,
+        staleTime: 300000,
+      },
+    },
+  })
+
   return (
-    <RecoilRoot>
-      <AnimatePresence exitBeforeEnter initial={true}>
-        <DefaultSeo {...SEO} />
-        <Head>
-          <link rel="icon" href="favicon.svg" type="image/svg+xml" />
-          <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" />
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <AnimatePresence exitBeforeEnter initial={true}>
+          <DefaultSeo {...SEO} />
+          <Head>
+            <link rel="icon" href="favicon.svg" type="image/svg+xml" />
+            <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" />
 
-          <meta httpEquiv="x-ua-compatible" content="IE=edge; chrome=1" />
-          <meta name="viewport" content="width=device-width,initial-scale=1" />
+            <meta httpEquiv="x-ua-compatible" content="IE=edge; chrome=1" />
+            <meta
+              name="viewport"
+              content="width=device-width,initial-scale=1"
+            />
 
-          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+            <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 
-          {/* Icon */}
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          />
+            {/* Icon */}
+            <link
+              rel="stylesheet"
+              href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            />
 
-          {/* Googleフォント */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lemonada:wght@300;400;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <Component {...pageProps} />
-      </AnimatePresence>
-    </RecoilRoot>
+            {/* Googleフォント */}
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;700&display=swap"
+              rel="stylesheet"
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;700&display=swap"
+              rel="stylesheet"
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Lemonada:wght@300;400;700&display=swap"
+              rel="stylesheet"
+            />
+          </Head>
+          <Component {...pageProps} />
+        </AnimatePresence>
+      </RecoilRoot>
+    </QueryClientProvider>
   )
 }
 
