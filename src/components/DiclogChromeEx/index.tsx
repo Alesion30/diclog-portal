12585fbf5bcab8eb0d1a4e1eb1c55a7fb9d94a4f
Yuta Ohira <alesion90@gmail.com>
2022-01-28 02:50:59 +0900

    :bug: onchageのエラーを修正

--- src/components/DiclogChromeEx/index.tsx
+++ src/components/DiclogChromeEx/index.tsx
@@ -97,6 +97,7 @@ export const DiclogChromeEx: React.VFC = () => {
               className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-yellow-600 focus:outline-none"
               rows={3}
               value={transWord}
+              onChange={() => null}
               placeholder="翻訳結果が表示されます"
             ></textarea>
           </form>
