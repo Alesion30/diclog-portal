522f56c2026a403927af317444c25aeac00e8ea9
Yuta Ohira <alesion90@gmail.com>
2022-01-31 20:29:36 +0900

    :zap: next-seoの設定ファイルをtypescript化

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,10 +1,12 @@
+import { DefaultSeoProps } from 'next-seo'
+
 // https://github.com/garmeeh/next-seo
-export default {
+const config: DefaultSeoProps = {
   title: 'Diclog（ディクログ）- まったく新しい英単語帳システム',
+  titleTemplate: '%s | Diclog（ディクログ）- まったく新しい英単語帳システム',
   description:
     'Diclogは自分の経験に基づいて、『こないだ調べたあの単語』を『復習』することによって記憶の定着を図る英単語帳です。しかも、単語登録の手間を完全に自動化してあります。DropboxやEvernoteを使っている人なら、自動同期の快適さは理解できると思います。最初にChrome Extensionを入れる必要がありますが、その後はそのExtensionが自動的に調べた英単語を記録してくれます。あとは、スマホでチェックして、消していくだけです。チェックするのは、トイレでも電車でも、ほんのちょっとしたスキマ時間です。',
   canonical: 'https://diclog-portal.vercel.app',
-  noindex: false,
   openGraph: {
     title: 'Diclog（ディクログ）- まったく新しい英単語帳システム',
     description:
@@ -19,3 +21,4 @@ export default {
     site_name: 'Diclog（ディクログ）',
   },
 }
+export default config
