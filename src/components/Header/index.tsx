f81b86602a934e422f464ac6b3ad8701fad9441e
Yuta Ohira <alesion90@gmail.com>
2022-01-31 19:37:13 +0900

    :zap: ヘッダーのリンクを修正

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,18 @@
+import Link from 'next/link'
 import { IconImage } from '~/components/Image'
 import { ScrollLink } from '../ScrollLink'
 
 export const Header: React.VFC = () => {
   return (
     <header className="bg-white w-full justify-between content-center rounded-full flex px-6 lg:px-14 py-5 shadow-lg">
-      <a href="/">
-        <div className="flex space-x-2 content-center">
-          <IconImage width={40} />
-          <h1 className="text-3xl font-bold font-lemon mt-1">Diclog</h1>
-        </div>
-      </a>
+      <Link href="/">
+        <a href="/">
+          <div className="flex space-x-2 content-center">
+            <IconImage width={40} />
+            <h1 className="text-3xl font-bold font-lemon mt-1">Diclog</h1>
+          </div>
+        </a>
+      </Link>
       <ul className="hidden lg:flex space-x-7 justify-center items-center">
         <li className="text-lg font-medium text-gray-500 hover:text-gray-900 cursor-pointer list-none">
           <ScrollLink to="about">Diclogとは</ScrollLink>
