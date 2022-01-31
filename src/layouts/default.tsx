f469d52028bc5a93995e2210f57a2a90f32d8d8c
Yuta Ohira <alesion90@gmail.com>
2022-01-31 20:52:35 +0900

    :bug: TOPに戻るボタンのバグを修正

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,11 +1,11 @@
 import { useScrollPosition } from '@n8tb1t/use-scroll-position'
 import clsx from 'clsx'
 import { useState } from 'react'
-import { ScrollLink } from '~/components/ScrollLink'
 import { Container } from '~/components/Container'
 import { Header } from '~/components/Header'
 import { Footer } from '~/components/Footer'
 import { motion } from 'framer-motion'
+import { animateScroll } from 'react-scroll'
 
 type LayoutProps = {
   children: React.ReactNode
@@ -23,6 +23,10 @@ export const DefaultLayout: React.VFC<LayoutProps> = ({ children }) => {
     setShowTopBtn(Math.abs(currPos.y) > 500)
   }, [])
 
+  const scrollToTop = () => {
+    animateScroll.scrollToTop()
+  }
+
   return (
     <motion.div
       animate={{ opacity: 1 }}
@@ -38,22 +42,23 @@ export const DefaultLayout: React.VFC<LayoutProps> = ({ children }) => {
             showTopBtn ? 'animate-fadeIn' : 'animate-fadeOut'
           )}
         >
-          <ScrollLink to="top">
-            <button className="bg-white hover:opacity-70 w-14 h-14 border-transparent shadow-lg rounded-full flex justify-center items-center">
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="chevron-up"
-                className="fill-amber-500 w-6 h-6 svg-inline--fa fa-chevron-up fa-w-14"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-              >
-                <path d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path>
-              </svg>
-            </button>
-          </ScrollLink>
+          <button
+            onClick={scrollToTop}
+            className="bg-white hover:opacity-70 w-14 h-14 border-transparent shadow-lg rounded-full flex justify-center items-center"
+          >
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="chevron-up"
+              className="fill-amber-500 w-6 h-6 svg-inline--fa fa-chevron-up fa-w-14"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <path d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path>
+            </svg>
+          </button>
         </div>
 
         {/* ヘッダー */}
