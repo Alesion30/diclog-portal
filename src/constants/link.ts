7250268f48b78793b1d72f52521d60662cf3f686
Yuta Ohira <alesion90@gmail.com>
2022-02-09 17:12:41 +0900

    :zap: リンクを短縮URLに修正

--- src/constants/link.ts
+++ src/constants/link.ts
@@ -1,13 +1,12 @@
 /** Apple Store */
-export const APPLE_STORE_LINK =
-  'https://apps.apple.com/app/diclog-%E8%8B%B1%E5%8D%98%E8%AA%9E%E5%B8%B3/id1603935437'
+export const APPLE_STORE_LINK = 'https://link.arakawa-lab.com/diclog-ios'
 
 /** Google Play Store */
 export const GOOGLE_PLAY_STORE_LINK = 'https://play.google.com/store/apps?hl=ja'
 
 /** Chrome Web Store */
 export const CHROME_WEB_STORE_LINK =
-  'https://chrome.google.com/webstore/detail/diclog%EF%BC%88%E3%83%87%E3%82%A3%E3%82%AF%E3%83%AD%E3%82%B0%EF%BC%89/dbhlboadooiimjhmldcfcbidkppocmie/related?hl=ja'
+  'https://link.arakawa-lab.com/diclog-chrome'
 
 /** Chrome拡張機能 ベータ版 */
 export const CHROME_EXT_BETA =
